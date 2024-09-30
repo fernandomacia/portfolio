a87b7e44fca2c67bc9461311044111ff432e9f86
Fernando Macia Lopez <fernando.macia@hotmail.es>
2024-09-30 16:49:56 +0200

    feat(header-intro): add header component
    - Created header-intro with profile image, text, and navigation links.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { HeaderIntroComponent } from './components/header-intro/header-intro.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        RouterOutlet,
+        HeaderComponent,
+        HeaderIntroComponent
+    ],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio';
+    title = 'portfolio';
 }
