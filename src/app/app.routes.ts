2ae308cd840733b0b308375b192cd25acdc05e96
Fernando Macia Lopez <fernando.macia@hotmail.es>
2024-10-10 16:25:27 +0200

    feat(habilidades): add routing and styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
 
 export const routes: Routes = [
     { path: 'inicio', component: InicioComponent },
     { path: 'proyectos', component: ProjectsComponent},
+    { path: 'habilidades', component: HabilidadesComponent},
     { path: '', redirectTo: '/inicio', pathMatch: 'full' }
 
 ];
